8267fe1f523325f7169fafa38e95ae758cdf9869
Marcia Rasi <marcia.pedals@gmail.com>
2026-02-20 14:04:41 -0800

    allow set/delete without confirm; implement multi-get; make prompt say what keys are being gotten

--- keymaster.swift
+++ keymaster.swift
@@ -45,69 +45,68 @@ func getPassword(key: String) -> String? {
 }
 
 func usage() {
-  print("keymaster [get|set|delete] [key] [secret]")
+  print("keymaster get [key1] [key2] ...")
+  print("keymaster set [key] [secret]")
+  print("keymaster delete [key]")
 }
 
 func main() {
   let inputArgs: [String] = Array(CommandLine.arguments.dropFirst())
-  if (inputArgs.count < 2 || inputArgs.count > 3) {
+  if (inputArgs.count < 2) {
     usage()
-    exit(EXIT_FAILURE) 
-  }
-  let action = inputArgs[0]
-  let key = inputArgs[1]
-  var secret = ""
-  if (action == "set" && inputArgs.count == 3) {
-    secret = inputArgs[2]
-  }
-
-  let context = LAContext()
-  context.touchIDAuthenticationAllowableReuseDuration = 0
-
-  var error: NSError?
-  guard context.canEvaluatePolicy(policy, error: &error) else {
-    print("This Mac doesn't support deviceOwnerAuthenticationWithBiometrics")
     exit(EXIT_FAILURE)
   }
+  let action = inputArgs[0]
 
   if (action == "set") {
-    context.evaluatePolicy(policy, localizedReason: "set to your password") { success, error in
-      guard setPassword(key: key, password: secret) else {
-        print("Error setting password")
-        exit(EXIT_FAILURE)
-      }
-      print("Key \(key) has been sucessfully set in the keychain")
-      exit(EXIT_SUCCESS)
+    guard inputArgs.count == 3 else {
+      usage()
+      exit(EXIT_FAILURE)
     }
-    dispatchMain()
-  }
-
-  if (action == "get") {
-    context.evaluatePolicy(policy, localizedReason: "access to your password") { success, error in
-      if success && error == nil {
-        guard let password = getPassword(key: key) else {
-          print("Error getting password")
-          exit(EXIT_FAILURE)
-        }
-        print(password)
-        exit(EXIT_SUCCESS)
-      } else {
-        let errorDescription = error?.localizedDescription ?? "Unknown error"
-        print("Error \(errorDescription)")
-        exit(EXIT_FAILURE)
-      }
+    let key = inputArgs[1]
+    let secret = inputArgs[2]
+    guard setPassword(key: key, password: secret) else {
+      print("Error setting password")
+      exit(EXIT_FAILURE)
     }
-    dispatchMain()
+    print("Key \(key) has been sucessfully set in the keychain")
+    exit(EXIT_SUCCESS)
   }
 
   if (action == "delete") {
-    context.evaluatePolicy(policy, localizedReason: "delete your password") { success, error in
+    guard inputArgs.count == 2 else {
+      usage()
+      exit(EXIT_FAILURE)
+    }
+    let key = inputArgs[1]
+    guard deletePassword(key: key) else {
+      print("Error deleting password")
+      exit(EXIT_FAILURE)
+    }
+    print("Key \(key) has been sucessfully deleted from the keychain")
+    exit(EXIT_SUCCESS)
+  }
+
+  if (action == "get") {
+    let keys = Array(inputArgs.dropFirst())
+    let keyList = keys.joined(separator: ", ")
+
+    let context = LAContext()
+    context.touchIDAuthenticationAllowableReuseDuration = 0
+
+    var error: NSError?
+    guard context.canEvaluatePolicy(policy, error: &error) else {
+      print("This Mac doesn't support deviceOwnerAuthenticationWithBiometrics")
+      exit(EXIT_FAILURE)
+    }
+
+    context.evaluatePolicy(policy, localizedReason: "access passwords for: \(keyList)") { success, error in
       if success && error == nil {
-        guard deletePassword(key: key) else {
-          print("Error deleting password")
-          exit(EXIT_FAILURE)
+        for key in keys {
+          if let password = getPassword(key: key) {
+            print("\(key)=\(password)")
+          }
         }
-        print("Key \(key) has been sucessfully deleted from the keychain")
         exit(EXIT_SUCCESS)
       } else {
         let errorDescription = error?.localizedDescription ?? "Unknown error"
